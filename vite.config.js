bb11390c87ff18d8d9b6ca6f2fff8baa7f8af00c
RafaelDeps <rafaeldeps15@gmail.com>
2026-07-15 21:46:04 -0300

    Add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using relative paths for asset loading on static hosting (GitHub Pages)
-});
+  base: '/steam_find_react/', 
+})
